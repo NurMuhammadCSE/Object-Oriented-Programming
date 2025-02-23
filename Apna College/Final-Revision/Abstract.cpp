2d8b5e2f9aa93462945784ac4ba1c8e0f3563abe
NurMuhammadCSE <nurmuhammadcse@gmail.com>
2025-02-23 23:09:20 +0600

    Abstraction

--- Apna College/Final-Revision/Abstract.cpp
+++ Apna College/Final-Revision/Abstract.cpp
@@ -10,11 +10,56 @@ public:
     void draw(){
         cout<<"Drawing a Circle"<<endl;
     }
+};
 
+void fun(){
+    // int x = 0; // 0 0 0
+    static int x = 0;
+    cout << x << endl;
+    x++;
+}
+
+class A{
+public:
+    int x;
+
+    void incX(){
+        x++;
+    }
+};
+
+class ABC{
+public:
+    ABC(){
+        cout << "Constructor" << endl;
+    }
+
+    ~ABC(){
+        cout << "Destructor" << endl;
+    }
 };
 
 int main(){
     Circle c;
     c.draw();
+
+    fun();
+    fun();
+    fun();
+
+    A a1;
+    a1.x = 10;
+    cout << a1.x << endl;
+
+    A a2;
+    a2.x = 20;
+    cout << a2.x << endl;
+
+
+    if(1){
+        // ABC obj;
+        static ABC obj;
+    }
+    cout << "End of Main Function" << endl;
     return 0;
 }
